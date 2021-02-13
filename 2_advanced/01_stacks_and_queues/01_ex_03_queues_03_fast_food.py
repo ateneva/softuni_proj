45297ccb7925bf2cf6dd6497d77c37fcdd24d859
ateneva <tenevaa21@gmail.com>
2021-02-13 23:50:06 +0100

    add problem statements

--- 2_advanced/01_stacks_and_queues/01_ex_03_queues_03_fast_food.py
+++ 2_advanced/01_stacks_and_queues/01_ex_03_queues_03_fast_food.py
@@ -2,6 +2,28 @@
 # ----------------------Problem 3: Fast Food------------------------------------
 # https://github.com/ateneva/softuni_proj/wiki/adv_queues_03_fast_food
 
+'''
+You have a fast food restaurant and most of the food that you're offering is
+previously prepared. You need to know if you will have enough food to serve
+lunch to all your customers.
+
+Write a program that checks the orders' quantity.
+You also want to know the client with the biggest order for the day
+
+First, you will be given the quantity of the food that you have for the day.
+Next, you will be given a sequence of integers, each representing the quantity of an order.
+
+Keep the orders in a queue.
+Find the biggest order and print it.
+You will begin servicing your clients from the first one that came.
+
+Before each order, check if you have enough food left to complete it.
+If you have, remove the order from the queue and reduce the amount of food you have.
+    If you succeeded in servicing all your clients, print: "Orders complete".
+    If not, print: "Orders left: {order1} {order2} .... {orderN}".
+
+'''
+
 # -----------100 points ---------------------
 from collections import deque
 
@@ -13,12 +35,12 @@ order_queue = deque(orders)
 print(max(orders))
 
 while orders:
-    order = orders.popleft()
+    order = orders.popleft()        # take the current order
     if food >= order:
         food -= order
     else:
         success = False
-        orders.appendleft(order)
+        orders.appendleft(order)    # keep the last remaining order
         break
 
 print_format = ' '.join(
--- 2_advanced/01_stacks_and_queues/01_ex_04_stacks_fashion_boutique.py
+++ 2_advanced/01_stacks_and_queues/01_ex_04_stacks_fashion_boutique.py
@@ -2,6 +2,24 @@
 # --------------------Problem 4 Fashion Boutique -------------------------------
 # https://github.com/ateneva/softuni_proj/wiki/adv_stacks_04_fashion_boutique
 
+'''
+You own a fashion boutique and you receive a delivery once a month in a huge box,
+which is full of clothes. You must arrange them in your store, so you take the box
+and start from the last piece of clothing on the top of the pile to the first one at the bottom.
+
+Use a stack for the purpose. Each piece of clothing has its value (an integer).
+You must sum their values, while you take them out of the box.
+You will be given an integer representing the capacity of a rack.
+    While the sum of the clothes is less than the capacity, keep summing them.
+    If the sum becomes equal to the capacity you must take a new rack for the next clothes,
+        if there are any left in the box.
+
+    If it becomes greater than the capacity,
+        don't add the piece of clothing to the current rack and take a new one.
+
+In the end, print how many racks you have used to hang must the clothes.
+'''
+
 # ----------100 points-------------------------
 
 clothes = [int(c) for c in input().split(' ')]
