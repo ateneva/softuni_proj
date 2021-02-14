96b56a3e0e8ce4655f4fdb1f312be0b01788bcb8
ateneva <tenevaa21@gmail.com>
2021-02-14 19:08:50 +0100

    add solution using set comprehensions

--- 2_advanced/02_tuples_and_sets/02_ex_02_sets_of_elements.py
+++ 2_advanced/02_tuples_and_sets/02_ex_02_sets_of_elements.py
@@ -6,13 +6,14 @@ Write a program that prints a set of elements.
 On the first line, you will receive two numbers - n and m,
 which represent the lengths of two separate sets.
 
-On the next n + m lines you will receive n numbers, which are the numbers in the 
+On the next n + m lines you will receive n numbers, which are the numbers in the
 first set, and m numbers, which are in the second set.
 
 Find all the unique elements that appear in both
     and print them on separate lines (the order does not matter).
 '''
 
+# approach 1: usng sets and loops
 nums = input().split(" ")
 
 set_one = int(nums[0])
@@ -31,3 +32,19 @@ for m in range(set_two):
 unique_set_numbers = set(a & b)
 for n in unique_set_numbers:
     print(n)
+
+# OR
+
+# approach 2: using set comprehensions
+def sets_of_elements(numbers):
+    nums = numbers.split(" ")
+    set_one = int(nums[0])
+    set_two = int(nums[1])
+
+    a = {int(input()) for n in range(set_one)}
+    b = {int(input()) for m in range(set_two)}
+
+    unique_set_numbers = set(a & b)
+    [print(n) for n in unique_set_numbers]
+
+sets_of_elements(input())
