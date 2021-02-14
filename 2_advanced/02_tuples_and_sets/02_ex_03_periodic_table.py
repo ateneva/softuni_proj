39e1bdaedbb391096f7a0ac7a5fd48973b75a9dc
ateneva <tenevaa21@gmail.com>
2021-02-14 19:43:50 +0100

    add a solution that uses union operator

--- 2_advanced/02_tuples_and_sets/02_ex_03_periodic_table.py
+++ 2_advanced/02_tuples_and_sets/02_ex_03_periodic_table.py
@@ -4,12 +4,13 @@
 '''
 Write a program that keeps all the unique chemical elements.
 On the first line you will be given a number n - the count of input lines
-that you are going to receive. On the next n lines, you will be receiving 
+that you are going to receive. On the next n lines, you will be receiving
 chemical compounds, separated by a single space.
 
 Your task is to print all the unique ones on separate lines
 '''
 
+# approach 1: use add set method
 n = int(input())
 
 unique_elements = set()
@@ -20,3 +21,19 @@ for _ in range(n):
 
 for e in unique_elements:
     print(e)
+
+# OR
+
+# approach 2: union a set of the subsequent elements with the original ones
+def periodic_table(e):
+    n = int(e)
+    unique_elements = set()
+
+    for _ in range(n):
+        elements = set(input().split(" "))
+        unique_elements |= elements                         # union operator
+        #unique_elements = unique_elements.union(elements)  # union method
+
+    print('\n'.join(unique_elements))
+
+periodic_table(input())
