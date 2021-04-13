71a98172871e4504a28d4f6c40655bc1d8c877c9
ateneva <tenevaa21@gmail.com>
2021-04-13 14:30:10 +0200

    2020-12-16-retake-exam-prolem-3

---
+++ 2_advanced/exam_prep/2020-12-16-03_problem_3.py
@@ -0,0 +1,31 @@
+
+'''
+Create a function called get_magic_triangle which will receive a single parameter
+ (integer n) and it should create a magic triangle which follows those rules:
+    - start with this simple triangle [[1], [1, 1]]
+    - generate the next rows until we reach n amount of rows
+    - Each number in each row is equal to the sum of the two numbers right above it in the triangle
+    - If the current number has no neighbor to the upper left/rigth, just take the existing neighbor
+
+'''
+
+def get_magic_triangle(n):
+    triangle = [[1], [1, 1]]
+
+    for row in range(2, n):
+        new_row = []
+        for col in range(0, row + 1):
+            if col - 1 < 0:
+                new_row.append(1)
+            elif col >= len(result[row-1]):
+                new_row.append(1)
+            else:
+                upper_left = result[row - 1][col - 1]
+                upper_right = result[row - 1][col]
+                new_box = upper_left + upper_right
+                new_row.append(new_box)
+
+        result.append(new_row)
+    return triangle
+
+print(get_magic_triangle(6))
