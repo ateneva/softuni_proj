f52912399a0df03ef41419e6e0b5cd5d6dd44eec
ateneva <tenevaa21@gmail.com>
2021-05-02 23:27:51 +0200

    class definition 3 classes

---
+++ 3_OOP/01_defining_classes/ex_08_spoopify/project/album.py
@@ -0,0 +1,55 @@
+from ex_08_spoopify.project.song import Song
+
+class Album:
+    def __init__(self, name, songs=None):
+        self.name = name
+
+        if songs is None:
+            self.songs = []
+
+        elif not isinstance(songs, list):
+            self.songs = []
+            self.songs.append(songs)
+        else:
+            self.songs = songs
+
+        self.published = False
+
+    def add_song(self, song: Song):
+        if song.single:
+            return f"Cannot add {song.name}. It's a single"
+
+        if self.published:
+            return "Cannot add songs. Album is published."
+
+        if song in self.songs:
+            return "Song is already in the album."
+
+        self.songs.append(song)
+        return f"Song {song.name} has been added to the album {self.name}."
+
+    def remove_song(self, song_name: str):
+        if song_name not in map(lambda s: s.name, self.songs):
+            return "Song is not in the album."
+
+        if self.published:
+            return "Cannot remove songs. Album is published."
+
+        song = list(filter(lambda x: x.name == song_name, self.songs))[0]
+        self.songs.remove(song)
+        return f"Removed song {song.name} from album {self.name}."
+
+    def publish(self):
+        if self.published:
+            return f"Album {self.name} is already published."
+
+        self.published = True
+        return f"Album {self.name} has been published."
+
+    def details(self):
+        result = f'Album {self.name}\n'
+
+        for s in self.songs:
+            result += f"== {s.get_info()}\n"
+
+        return result
