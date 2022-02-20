48cf17fd3b922af621f28075aa477ad610fa2664
ateneva <tenevaa21@gmail.com>
2022-02-20 13:53:21 +0100

    CU-hz960x: declare the web response as avariable

--- 4_web_basics/lab_urls/employees/views.py
+++ 4_web_basics/lab_urls/employees/views.py
@@ -3,8 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+# A function is a Django view if:
+# * accepts HTTPrequest as a first parameter
+# * returns an HTTPresponse
+
 def home_page(request):
-	return HttpResponse('The App works')
+	html = f'<h1>{request.method}: The App works</h1>'
+	return HttpResponse(html)
 
 
 def department_list(request):
@@ -14,5 +19,3 @@ def department_list(request):
 # create a dynamic view by using a parameter
 def department_details(request, id):
 	return HttpResponse(f'This is department {id} page')
-
-
