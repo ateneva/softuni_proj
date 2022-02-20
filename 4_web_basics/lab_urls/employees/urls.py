319b743304f37c370d4e5aa96656aa078295359a
ateneva <tenevaa21@gmail.com>
2022-02-20 15:07:29 +0100

    CU-hz960x: add view that raises exceptions

--- 4_web_basics/lab_urls/employees/urls.py
+++ 4_web_basics/lab_urls/employees/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from employees import views
 
-# Mandatory tuple or list
+# urlpatterns is mandatory tuple or list
 urlpatterns = [
     # path('department/', views.department, name='department'),   # localhost:8000/department
     # whether or not paths should be defined with an ending slash is defined in SETTINGS with the APPEND_SLASH
     path('', views.department_list),
-    path('/<int:id>', views.department_details),
+    #path('/<int:id>', views.department_details),
+    path('/<int:department_id>', views.department_employees),
 
 
     # instead of writing departments multiple times in here, it's better to include it as a prefix in the project urls
@@ -14,5 +15,3 @@ urlpatterns = [
     # path('departments/update', views.update_department),
     # path('departments/delete', views.delete_department),
 ]
-
-
--- 4_web_basics/lab_urls/employees/views.py
+++ 4_web_basics/lab_urls/employees/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
@@ -9,7 +9,13 @@ from django.http import HttpResponse
 
 def home_page(request):
 	html = f'<h1>{request.method}: The App works</h1>'
-	return HttpResponse(html)
+	return HttpResponse(
+			html,
+			#content_type = 'text/plain',
+			headers={
+				'x-annie-header': 'Django',
+			},
+		)
 
 
 def department_list(request):
@@ -19,3 +25,28 @@ def department_list(request):
 # create a dynamic view by using a parameter
 def department_details(request, id):
 	return HttpResponse(f'This is department {id} page')
+
+
+# raise exceptions
+def department_employees(request, department_id):
+	employee = None
+
+	if department_id > 0 and department_id < 3:
+		if department_id == 1:
+			employee = "Peter Smith"
+
+		elif department_id == 2:
+			employee = "Mariya Brown"
+
+		html = "<html><body><h1>Employee: %s, " \
+			   "Department: %s</h1></body></html>" \
+			   % (employee, department_id)
+
+		return HttpResponse(html)
+	else:
+		return HttpResponseNotFound('<h1>Department was not found</h1>')
+
+	# return HTTPresponseNotFound('Department was not found')
+		# is the same as
+	# return HTTPresponse(status=404)
+	# raise Http404
