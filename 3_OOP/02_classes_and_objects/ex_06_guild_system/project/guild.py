d626b72164d21945dbad0a917fd42def9b4cfe0f
ateneva <tenevaa21@gmail.com>
2022-03-12 17:02:34 +0100

    CU-ez3vu3: solve-ex-06-guild-system

---
+++ 3_OOP/02_classes_and_objects/ex_06_guild_system/project/guild.py
@@ -0,0 +1,61 @@
+'''
+The Guild class receives a name (string). The Guild should also have one instance attribute players (an empty list which will contain the players of the guild). The class also has 3 additional methods:
+-	assign_player(player: Player)
+o	Adds the player to the guild and returns "Welcome player {player_name} to the guild {guild_name}". Remember to change the player's guild in the player class.
+o	If he is already in the guild, returns "Player {player_name} is already in the guild."
+o	If the player is in another guild, returns "Player {player_name} is in another guild."
+-	kick_player(player_name: str)
+o	Removes the player from the guild and returns "Player {player_name} has been removed from the guild.". Remember to change the player's guild in the player class to "Unaffiliated".
+o	If there is no such player in the guild, returns "Player {player_name} is not in the guild."
+-	guild_info()
+o	Returns the guild's information, including the players in the guild, in the format:
+"Guild: {guild_name}
+{first_player's info}
+…
+{Nplayer's info}"
+'''
+
+from project.player import Player
+#from player import Player
+
+class Guild:
+
+    def __init__(self, name: str):
+        self.name = name
+        self.players = []
+
+    def assign_player(self, player: Player):
+        if player.guild == self.name:
+            return f"Player {player.name} is already in the guild."
+
+        if player.guild != player.DEFAULT_GUILD:
+            return f"Player {player.name} is in another guild."
+
+        self.players.append(player)
+        player.guild = self.name
+        return f"Welcome player {player.name} to the guild {self.name}"
+
+    def kick_player(self, player_name: str):
+        if player_name in self.players:
+            self.players.remove(player_name)
+            player.guild = player.DEFAULT_GUILD
+            return f"Player {player.name} has been removed from the guild."
+        return f"Player {player.name} is not in the guild."
+
+    def guild_info(self):
+        result = f'Guild: {self.name}'
+        for player in self.players:
+            result += '\n' + player.player_info()
+        return result
+
+player = Player("George", 50, 100)
+player2 = Player("Ivan", 10, 30)
+print(player.add_skill("Shield Break", 20))
+print(player.player_info())
+guild = Guild("UGT")
+print(guild.assign_player(player))
+print(guild.guild_info())
+
+print(guild.assign_player(player2))
+print(guild.kick_player(player2))
+print(guild.guild_info())
---
+++ 3_OOP/02_classes_and_objects/ex_06_guild_system/project/player.py
@@ -0,0 +1,51 @@
+'''
+The Player class should receive a name (string), a hp (int), and a mp (int) upon initialization. The Player also has 2 instance attributes: skills (empty dictionary that will contain the skills of each player and its mana cost) and a guild set to "Unaffiliated" by default.
+The Player class should also have two additional methods:
+-	add_skill(skill_name, mana_cost)
+o	Adds the skill and the corresponding mana cost to the dictionary of skills. Returns "Skill {skill_name} added to the collection of the player {player_name}"
+o	If the skill is already in the collection, returns "Skill already added"
+-	player_info()
+o	Returns the player's information, including their skills, in this format:
+"Name: {player_name}
+ Guild: {guild_name}
+ HP: {hp}
+ MP: {mp}
+ ==={skill_name_1} - {skill_mana_cost}
+ ==={skill_name_2} - {skill_mana_cost}
+ …
+ ==={skill_name_N} - {skill_mana_cost}"
+'''
+
+class Player:
+    DEFAULT_GUILD = "Unaffiliated"
+
+    def __init__(self, name: str, hp: int, mp: int):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.skills = {}
+        self.guild = Player.DEFAULT_GUILD
+
+    def add_skill(self, skill_name, mana_cost):
+        if skill_name in self.skills:
+            return "Skill already added"
+
+        self.skills[skill_name] = mana_cost
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
+
+    def player_info(self):
+        result = f'Name: {self.name}\n'
+        result += f'Guild: {self.guild}\n'
+        result += f'HP: {self.hp}\n'
+        result += f'MP: {self.mp}\n'
+        result += "".join([f'==={skill} - {cost}\n' for skill, cost in self.skills.items()])
+
+
+        return result
+
+
+#player = Player("George", 50, 100)
+#print(player.add_skill("Shield Break", 20))
+#print(player.add_skill("Save Relationship", 100))
+#print(player.add_skill("Understand Enemy", 50))
+#print(player.player_info())
