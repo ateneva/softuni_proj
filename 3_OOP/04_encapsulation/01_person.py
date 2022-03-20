1a89aad7ab818c0cf7befc1e89e39e7854ec3b33
ateneva <tenevaa21@gmail.com>
2022-03-20 12:03:47 +0100

    CU-hx33du: specify condition-better

--- 3_OOP/04_encapsulation/01_person.py
+++ 3_OOP/04_encapsulation/01_person.py
@@ -1,9 +1,12 @@
 
 '''
 
-* Create a class called Person. Upon initialization it should receive name and age
-* Create name mangled properties - name and age
-* Create get_name and get_age methods to return their values
+Create a class called Person.
+Upon initialization, it should receive a name and an age.
+Name mangle the name and the age attributes (should not be accessed outside the class).
+
+Create two instance methods called get_name and get_age
+to return the values of the private attributes.
 
 '''
 
